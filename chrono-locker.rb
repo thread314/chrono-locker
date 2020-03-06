6d4c5401271e1e23478f85bf62ebefaeb9e0ba74
Eddie Drury <eddie.drury@gmail.com>
2020-03-06 10:48:29 +0800

    open file prompt finished and keep copy of key method finished

--- chrono-locker.rb
+++ chrono-locker.rb
@@ -1,33 +1,69 @@
 require 'openssl'
-data = "Very, very confidential data"
 
 cipher = OpenSSL::Cipher::AES.new(256, :CBC)
 cipher.encrypt
-key = cipher.random_key
+$key = cipher.random_key
 iv = cipher.random_iv
+puts "Welcome to Chrono-Locker."
+puts "Please enter the name of the file you would like to encrypt..."
 
-encrypted = cipher.update(data) + cipher.final
-puts key
-keyasinteger = key.unpack('H*').first
-keyasinteger = keyasinteger.to_i(16)
-
-puts "**************"
-
-decipherkey = keyasinteger 
-decipherkey = decipherkey.to_s(16)
-
-if decipherkey.length < 64
-   decipherkey = "0#{decipherkey}"
+def openfile
+  $filetoencrypt = gets.chomp
+  begin
+    puts File.open($filetoencrypt, "r")
+  rescue
+    puts "Invalid filename"
+    openfile
+  end
 end
 
-decipherkey = [decipherkey].pack('H*')
-puts decipherkey
+openfile
 
-decipher = OpenSSL::Cipher::AES.new(256, :CBC)
-decipher.decrypt
-decipher.key = decipherkey
-decipher.iv = iv
+puts $filetoencrypt
 
-plain = decipher.update(encrypted) + decipher.final
+def keepkey 
+  puts "Would you like to keep a copy of the key? (y/n)"
+  keepkeyanswer = gets.chomp
+  if keepkeyanswer == "y" 
+    keyfile = "#{$filetoencrypt}.key"
+    keyfile = File.new("#{$filetoencrypt}.key", "w")
+    keyfile.write($key)
+    keyfile.close
+    puts "Saving decryption key as #{keyfile}"
+  elsif keepkeyanswer == "n"
+    puts "WARNING: the decryption key will not be saved. The only way to decrypt the file will be to brute-force it."
+  else
+    puts "Invalid answer. Please try again."
+    keepkey
+  end
+end
 
-puts data == plain #=> true
+keepkey
+
+
+#
+#encrypted = cipher.update(data) + cipher.final
+#puts $key
+#keyasinteger = $key.unpack('H*').first
+#keyasinteger = $keyasinteger.to_i(16)
+#
+#puts "**************"
+#
+#decipherkey = keyasinteger 
+#decipherkey = decipherkey.to_s(16)
+#
+#if decipherkey.length < 64
+#   decipherkey = "0#{decipherkey}"
+#end
+#
+#decipherkey = [decipherkey].pack('H*')
+#puts decipherkey
+#
+#decipher = OpenSSL::Cipher::AES.new(256, :CBC)
+#decipher.decrypt
+#decipher.$key = decipherkey
+#decipher.iv = iv
+#
+#plain = decipher.update(encrypted) + decipher.final
+#
+#puts data == plain #=> true
--- scratch.rb
+++ scratch.rb
@@ -1,34 +1,39 @@
 require 'openssl'
 
+
+
+
+
 # encryption
-cipher = OpenSSL::Cipher.new('aes-256-cbc')
-cipher.encrypt
-key = cipher.random_key
-iv = cipher.random_iv
-
-buf = ""
-File.open("file.enc", "wb") do |outf|
-  File.open("file", "rb") do |inf|
-    while inf.read(4096, buf)
-      outf << cipher.update(buf)
-    end
-    outf << cipher.final
-  end
-end
-
-
-# decryption
-cipher = OpenSSL::Cipher.new('aes-256-cbc')
-cipher.decrypt
-cipher.key = key
-cipher.iv = iv # key and iv are the ones from above
-
-buf = ""
-File.open("file.dec", "wb") do |outf|
-  File.open("file.enc", "rb") do |inf|
-    while inf.read(4096, buf)
-      outf << cipher.update(buf)
-    end
-    outf << cipher.final
-  end
-end
+#cipher = OpenSSL::Cipher.new('aes-256-cbc')
+#cipher.encrypt
+#key = cipher.random_key
+#iv = cipher.random_iv
+#
+#output = File.new("output.enc", "w")
+#buf = ""
+#File.open("output.enc", "wb") do |outf|
+#  File.open("file", "rb") do |inf|
+#    while inf.read(4096, buf)
+#      outf << cipher.update(buf)
+#    end
+#    outf << cipher.final
+#  end
+#end
+#
+#
+## decryption
+#cipher = OpenSSL::Cipher.new('aes-256-cbc')
+#cipher.decrypt
+#cipher.key = key
+#cipher.iv = iv # key and iv are the ones from above
+#
+#buf = ""
+#File.open("file.dec", "wb") do |outf|
+#  File.open("output.enc", "rb") do |inf|
+#    while inf.read(4096, buf)
+#      outf << cipher.update(buf)
+#    end
+#    outf << cipher.final
+#  end
+#end
